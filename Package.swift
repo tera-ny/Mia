fcf509081f980f97da865e5521ac99f176ab4033
g4zeru <haru25haru02@gmail.com>
2020-09-26 18:03:50 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Mia",
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .branch("6.33-spm-beta")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Mia",
-            dependencies: []),
+            dependencies: [.product(name: "FirebaseFirestore", package: "Firebase"), .product(name: "FirebaseFirestoreSwift", package: "Firebase")]),
         .testTarget(
             name: "MiaTests",
             dependencies: ["Mia"]),
